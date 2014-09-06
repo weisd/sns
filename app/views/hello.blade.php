9d982064d053dc4a07107c17a1ab1f655b285ca7
weisd <weishidavip@163.com>
2014-09-07 01:03:10 +0800

    注册，登陆，退出 ，找回密码，重设密码 ok , 待测试

---
+++ app/views/hello.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="zh"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <style type="text/css"></style><style id="holderjs-style" type="text/css"></style></head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+         <!--  <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Cover</h3>
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div> -->
+
+          <div class="inner cover">
+            <h1 class="cover-heading">weisd/sns</h1>
+            <p class="lead"></p>
+            <p class="lead">
+              <a href="http://code.weisd.in/weisd/sns-doc" class="btn btn-lg btn-default">Learn more</a>
+            </p>
+          </div>
+          <div class="mastfoot">
+            <div class="inner">
+              <p> by <a href="weisd.in">@weisd</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="bootstrap-3.2.0/js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  
+
+<div id="GWDANG_HAS_BUILT"></div></body></html>
---
+++ app/views/site/signin.blade.php
@@ -0,0 +1,14 @@
+{{$errors->first('error', '<strong>:message</strong>')}}
+
+{{Form::open()}}
+{{Form::label('email', '邮箱地址')}}
+{{Form::text('email')}}
+
+{{Form::label('password', '密码')}}
+{{Form::password('password')}}
+
+{{Form::label('remember', '记住我')}}
+{{Form::checkbox('remember')}}
+
+{{Form::submit('登陆')}}
+{{Form::close()}}
